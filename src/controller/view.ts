1dd6089f2709728bd900c6303e1b9df11be1ccea
Shahar Best <shaharb@localize.city>
2021-12-29 13:58:05 +0200

    view cache

--- src/controller/view.ts
+++ src/controller/view.ts
@@ -5,69 +5,46 @@ import config from '../config'
 import sharp, { Sharp } from 'sharp'
 import { validateCommand, getExecuter, executer } from '../transformations/transformer'
 
-export async function view(req: Request, res: Response) {
-    const filePath = path.join(config.uploadDirPath, req.params.file_name)
+export function view(viewRequests: Map<string, Sharp>) {
+    return (req: Request, res: Response) => {
+        const filePath = path.join(config.uploadDirPath, req.params.file_name)
 
-    // check if request image exist
-    try {
-        if (!fs.existsSync(filePath)) {
-            res.status(404).send('image not found')
+        // check if request image exist
+        try {
+            if (!fs.existsSync(filePath)) {
+                res.status(404).send('image not found')
+                return
+            }
+        } catch (err) {
+            res.status(500).send('something went wrong')
             return
         }
-    } catch (err) {
-        res.status(500).send('something went wrong')
-        return
-    }
 
-    const commands: string[] = req.params.trans_list.split(';')
+        const commands: string[] = req.params.trans_list.split(';')
 
-    // check commands validation
-    // const errors: string[] =
-    //     commands.filter(curCommand => validateCommand(curCommand))
-    const errors: string[] =
-        commands.map(curCommand => validateCommand(curCommand)).filter(curError => curError != '')
+        // check commands validation
+        const errors: string[] =
+            commands.map(curCommand => validateCommand(curCommand)).filter(curError => curError != '')
 
-    if (errors.length > 0) {
+        if (errors.length > 0) {
 
-        res.status(400).send(errors.join(',\n'))
-        return
-    }
-    // const invalidCommand: string | undefined = findInvalidCommand(commands)
+            res.status(400).send(errors.join(',\n'))
+            return
+        }
 
-    // if (invalidCommand) {
-    //     res.status(400).send(`unknown command: ${invalidCommand}`)
-    //     return
-    // }
-
-    // execute commands
-    try {
-        const finalImage = await executeCommands(commands, sharp(filePath))
-        finalImage.pipe(res)
-    } catch (error) {
-        res.status(500).send(error)
+        // execute commands
+        try {
+            const finalImage: Sharp = executeCommands(commands, sharp(filePath))
+            viewRequests.set(req.url, finalImage)
+            finalImage.pipe(res)
+        } catch (error) {
+            res.status(500).send(error)
+        }
     }
 }
 
-async function executeCommands(commands: string[], rawImage: Sharp): Promise<Sharp> {
+function executeCommands(commands: string[], rawImage: Sharp): Sharp {
     const executersList: executer[] = commands.map(curCommand => getExecuter(curCommand))
 
     return executersList.reduce((finalResult, curExecuter) => curExecuter(finalResult), rawImage)
 }
-
-function findInvalidCommand(commands: string[]): string | undefined {
-    return commands.find(curCommand => validateCommand(curCommand))
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- src/routes/view.ts
+++ src/routes/view.ts
@@ -1,8 +1,14 @@
 import express, { Router } from 'express'
 import { view } from '../controller/view'
+import viewCache from '../middlewares/viewCache'
+import { Sharp } from 'sharp'
 
 const router: Router = express.Router()
+const interval: number = parseInt(process.env.CACHE_INTERVAL)
+const viewRequests: Map<string, Sharp> = new Map()
 
-router.get('/:trans_list/:file_name', view)
+setInterval(() => viewRequests.clear(), interval * 1000)
+
+router.get('/:trans_list/:file_name', viewCache(viewRequests), view(viewRequests))
 
 export default router
