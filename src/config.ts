20f792a8daf222ad703e4ebf048fdbdfae4b997e
Shahar Best <shaharb@localize.city>
2022-01-03 15:56:36 +0200

    allowed file extensions in config.json

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,8 @@
-import path from "path"
 import dotenv from 'dotenv';
 dotenv.config()
+
+import path from "path"
+import config from './config.json'
 
 export type Config = {
     uploadDirPath: string,
@@ -17,12 +19,12 @@ const { DB_USER: dbUser,
     PORT: port,
     ACCESSS_TOKEN_SECRET: accessTokenSecret } = process.env
 
-const exts: string[] = ['jpg', 'jpeg', 'png']
-const str: string = exts.map(curExt => `\\.${curExt}`).join('|')
+const str: string = config.allowedFormats.map(curExt => `\\.${curExt}`).join('|')
 const extname_pattern: RegExp = new RegExp(`^(${str})$`)
 const uploadDirPath: string = path.join(__dirname, '..', process.env.UPLOAD_DIR_NAME)
 const dbUrl: string =
     `mongodb://${dbUser}:${dbPassword}@${dbHost}:${dbPort}/imaginary?authSource=admin`
 
-const config: Config = { uploadDirPath, port, extname_pattern, dbUrl, accessTokenSecret }
-export default config
+const exportedConfig: Config =
+    { uploadDirPath, port, extname_pattern, dbUrl, accessTokenSecret }
+export default exportedConfig
